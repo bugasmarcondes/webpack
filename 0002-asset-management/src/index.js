ae598bcc44b521779ca24a211b2733f4134c0622
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-25 14:26:09 -0300

    - Carregando css - Carregando imagens - Carregando fontes (chrome deve ser woff2 e woff) - Carregando csv e tsv - Carregando xml

--- 0002-asset-management/src/index.js
+++ 0002-asset-management/src/index.js
@@ -1,21 +1,27 @@
-import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
+import _ from 'lodash';
+// 1. carregando css
+import './style.css';
+// 2. carregando imagens
+import Icon from './icon.png';
+// 4. carregando csv e tsv
+// OBS: Only the default export of JSON modules can be used without warning.
+import Data from './data.xml';
 
 function component() {
-  const element = document.createElement("div");
+    const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+    element.appendChild(myIcon);
+    console.log(Data);
 
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
--- 0002-asset-management/webpack.config.js
+++ 0002-asset-management/webpack.config.js
@@ -1,21 +1,50 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
-      }
-    ]
-  }
+    entry: './src/index.js',
+    // setup
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            // 1. carregando css
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            // 2. carregando imagens
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            // 3. carregando fontes (chrome deve ser woff2 e woff)
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            // 4. carregando csv e tsv
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader',
+                ],
+            },
+            // carregando xml
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
+                ],
+            },
+        ],
+    },
 };
